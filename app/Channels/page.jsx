9bee63bdc85bb47a2ce22a0345406ee7d5d8130a
Rao Shahbaz <shahbazdevloper@gmail.com>
2025-08-24 12:13:43 +0500

    Section Updated

--- app/Channels/page.jsx
+++ app/Channels/page.jsx
@@ -12,7 +12,7 @@ export default function Page() {
         <main>
             <Header />
             <ServiceBanner />
-            {/* <ChannelsSection /> */}
+            <ChannelsSection />
               <MoviesSection />
             <Count />
             <FeedbackForm />
